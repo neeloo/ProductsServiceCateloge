b2ce12dfc744d345eeeffd796dee7b98404bec32
neeloo patel <nilusingh945164@gmail.com>
2026-08-03 17:22:13 +0530

    backend

--- src/main/java/com/Neeloo/ProductsServiceCateloge/services/ProductService.java
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.Neeloo.ProductsServiceCateloge.services;
 
 import com.Neeloo.ProductsServiceCateloge.models.Product;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,5 +12,13 @@ public interface ProductService {
     Product getsingleProduct(Long id);
 
     List<Product> getAllProducts();
+
+    Product  updateProduct(Long id , Product product);
+
+    Product replaceProduct(Long id , Product product);
+
+    Product addNewProduct(Product product);
+
+    boolean deleteProduct(Long id);
 
 }
