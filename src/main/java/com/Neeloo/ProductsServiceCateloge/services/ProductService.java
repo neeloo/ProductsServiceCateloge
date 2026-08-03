571bd0582659e599a916a2dadafb9f68ffe4f183
neeloo patel <nilusingh945164@gmail.com>
2026-08-03 17:14:37 +0530

    backend

--- src/main/java/com/Neeloo/ProductsServiceCateloge/services/ProductService.java
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/services/ProductService.java
@@ -1,7 +1,15 @@
 package com.Neeloo.ProductsServiceCateloge.services;
 
+import com.Neeloo.ProductsServiceCateloge.models.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductService {
+public interface ProductService {
+
+    Product getsingleProduct(Long id);
+
+    List<Product> getAllProducts();
+
 }
