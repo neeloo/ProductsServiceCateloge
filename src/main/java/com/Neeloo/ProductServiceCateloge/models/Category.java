eb10d9b978c19ce47c90ff5c2476f85e1960997c
neeloo patel <nilusingh945164@gmail.com>
2026-08-03 16:27:51 +0530

    backend

--- src/main/java/com/Neeloo/ProductServiceCateloge/models/Category.java
+++ src/main/java/com/Neeloo/ProductServiceCateloge/models/Category.java
@@ -1,4 +1,19 @@
 package com.Neeloo.ProductServiceCateloge.models;
 
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
 public class Category extends  BaseModel{
+
+    private List<Product> products;
+    private String name;
+    private String description;
+    private String imageUrl;
 }
--- src/main/java/com/Neeloo/ProductServiceCateloge/models/Product.java
+++ src/main/java/com/Neeloo/ProductServiceCateloge/models/Product.java
@@ -1,4 +1,19 @@
 package com.Neeloo.ProductServiceCateloge.models;
 
-public class Product {
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Product  extends  BaseModel{
+
+    private String title;
+    private double price;
+    private Category category;
+    private String description;
+    private String imageUrl;
+    private int numberOfSales;
 }
