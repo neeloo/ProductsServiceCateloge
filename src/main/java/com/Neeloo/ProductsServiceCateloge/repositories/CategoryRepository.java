eea61fe388be3d02578f54fe89fe2316021640fd
neeloo patel <nilusingh945164@gmail.com>
2026-08-03 17:07:27 +0530

    backend

--- src/main/java/com/Neeloo/ProductsServiceCateloge/repositories/CategoryRepository.java
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/repositories/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.Neeloo.ProductsServiceCateloge.repositories;
 
 
+import com.Neeloo.ProductsServiceCateloge.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CategoryRepository {
+public interface CategoryRepository  extends JpaRepository<Category ,Long> {
 }
--- src/main/java/com/Neeloo/ProductsServiceCateloge/repositories/ProductRepository.java
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/repositories/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.Neeloo.ProductsServiceCateloge.repositories;
 
 
+import com.Neeloo.ProductsServiceCateloge.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
