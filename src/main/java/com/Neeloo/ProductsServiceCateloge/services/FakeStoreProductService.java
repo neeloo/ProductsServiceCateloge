b8691f4de77e9d571df88a31305962ae28563424
neeloo patel <nilusingh945164@gmail.com>
2026-08-04 15:15:13 +0530

    backend

--- src/main/java/com/Neeloo/ProductsServiceCateloge/services/FakeStoreProductService.java
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/services/FakeStoreProductService.java
@@ -1,8 +1,21 @@
 package com.Neeloo.ProductsServiceCateloge.services;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FakeStoreProductService {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public FakeStoreProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+
+
+
 }
