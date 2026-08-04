d29f7c09c843d18fb63c5966ef37b093bdcbc94d
neeloo patel <nilusingh945164@gmail.com>
2026-08-04 15:20:41 +0530

    backend

--- src/main/java/com/Neeloo/ProductsServiceCateloge/services/FakeStoreProductService.java
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/services/FakeStoreProductService.java
@@ -1,6 +1,9 @@
 package com.Neeloo.ProductsServiceCateloge.services;
 
 
+import com.Neeloo.ProductsServiceCateloge.dtos.FakeStoreProductDto;
+import com.Neeloo.ProductsServiceCateloge.models.Category;
+import com.Neeloo.ProductsServiceCateloge.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +16,19 @@ public class FakeStoreProductService {
     @Autowired
     public FakeStoreProductService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    private Product convertFakeStorePRoductDtoProduct(FakeStoreProductDto fakeStoreProductDto){
+        Product product = new Product();
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setId(fakeStoreProductDto.getId());
+        product.setPrice(fakeStoreProductDto.getPrice());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setImageUrl(fakeStoreProductDto.getImage());
+        product.setCategory(new Category());
+        product.getCategory().setName(fakeStoreProductDto.getCategory());
+
+        return  product;
     }
 
 
