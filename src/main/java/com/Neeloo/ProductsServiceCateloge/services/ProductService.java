429fa14e3a2702b153b14f5d838bc1899b25cd17
neeloo patel <nilusingh945164@gmail.com>
2026-08-09 14:06:13 +0530

    backend

--- src/main/java/com/Neeloo/ProductsServiceCateloge/services/ProductService.java
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/services/ProductService.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 public interface ProductService {
 
     Product getsingleProduct(Long id);
