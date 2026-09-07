11e40864c9d9dbd92fb0ef6e2a7c21a59b8d1f0b
neeloo patel <nilusingh945164@gmail.com>
2026-09-07 10:38:37 +0530

    backend

---
+++ src/main/java/com/Neeloo/ProductsServiceCateloge/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.Neeloo.ProductsServiceCateloge;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
